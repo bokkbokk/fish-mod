abc8e97499a04c95fc988dec0f8ba5040ccc2c48
bokkbokk <tuckerwarnke@gmail.com>
2025-05-24 22:45:25 -0600

    Initial commit

---
+++ src/main/java/net/bokkbokk/fishmod/FishModClient.java
@@ -0,0 +1,10 @@
+package net.bokkbokk.fishmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FishModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
