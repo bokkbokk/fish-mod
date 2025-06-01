20333395cedf29a8a3d3b3145aaf883ba4bf705d
bokkbokk <tuckerwarnke@gmail.com>
2025-06-01 12:37:16 -0600

    fish gem oregen in fish biome

--- src/main/java/net/bokkbokk/fishmod/world/gen/ModOreGeneration.java
+++ src/main/java/net/bokkbokk/fishmod/world/gen/ModOreGeneration.java
@@ -1,6 +1,7 @@
 package net.bokkbokk.fishmod.world.gen;
 
 import net.bokkbokk.fishmod.world.ModPlacedFeatures;
+import net.bokkbokk.fishmod.world.biome.ModBiomes;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.world.biome.BiomeKeys;
@@ -9,7 +10,7 @@ import net.minecraft.world.gen.GenerationStep;
 public class ModOreGeneration {
 
     public static void generateOres(){
-        BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.WARM_OCEAN)
+        BiomeModifications.addFeature(BiomeSelectors.includeByKey(ModBiomes.FISH_BIOME)
                 ,GenerationStep.Feature.UNDERGROUND_ORES
                 ,ModPlacedFeatures.FISH_ORE_BLOCK_PLACED_KEY);
 
